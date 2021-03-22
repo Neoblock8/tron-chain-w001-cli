c9048abb39080c03e4364f09db556c36a019fb85
Neoblock8 <neoblock8@gmail.com>
2021-03-22 11:43:38 +0530

    Wyzth-wallet-cli

---
+++ src/main/java/org/tron/core/exception/EncodingException.java
@@ -0,0 +1,11 @@
+package org.tron.core.exception;
+
+public class EncodingException extends WyzthException {
+  public EncodingException() {
+    super();
+  }
+
+  public EncodingException(String msg) {
+    super(msg);
+  }
+}
---
+++ src/main/java/org/tron/core/exception/WyzthException.java
@@ -0,0 +1,17 @@
+package org.tron.core.exception;
+
+public class WyzthException extends Exception {
+
+  public WyzthException() {
+    super();
+  }
+
+  public WyzthException(String message) {
+    super(message);
+  }
+
+  public WyzthException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
